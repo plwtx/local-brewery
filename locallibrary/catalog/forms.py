d21031b0b6413bbebc64736055631f4ae109a57b
plwtx <h.lenny933@gmail.com>
2025-06-01 00:47:20 +0200

    0.2.2.0 | Before new model

--- locallibrary/catalog/forms.py
+++ locallibrary/catalog/forms.py
@@ -11,8 +11,13 @@ class RegistrationForm(UserCreationForm):
         model = get_user_model()
         fields = ("username", "email", "first_name", "last_name", "password1", "password2")
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password2'].label = "Confirm pass"
+        self.fields['password2'].help_text = "Enter the same password as before, for verification."
+
     def save(self, commit=True):
-        user = super(RegistrationForm, self).save(commit=False)
+        user = super().save(commit=False)
         user.email = self.cleaned_data["email"]
         user.first_name = self.cleaned_data["first_name"]
         user.last_name = self.cleaned_data["last_name"]
