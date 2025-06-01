427d5a0204199824e1ef4b2541e5de0242dff4eb
plwtx <h.lenny933@gmail.com>
2025-06-02 01:40:09 +0200

    0.2.3 | Stable ver.

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -30,7 +30,7 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('index')
+            return redirect('home')
     else:
         form = RegistrationForm()
     return render(request, 'registration/register.html', {'form': form})
